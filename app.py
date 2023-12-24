6d2e952ad664079a54642b8ad5adb53d3f790744
Akramerabti <akramerabti@gmail.com>
2023-12-23 23:35:53 -0500

    s

--- app.py
+++ app.py
@@ -1,30 +1,56 @@
-from flask import Flask, render_template, request, redirect, url_for, jsonify, make_response
-from flask_cors import CORS
-from pymongo import MongoClient
-app = Flask(__name__)
+from urllib.request import urlopen, Request
+from bs4 import BeautifulSoup
 
+finviz_url = 'https://finviz.com/quote.ashx?t='
 
-CORS(app, resources={r"/predict": {"origins": "*"}})
+tickers = ['AMZN', 'GOOG', 'FB']
 
-client = MongoClient("mongodb+srv://Akramvd:lF9UjtVXF0iWsxetr2MK@cluster0.7wctpqm.mongodb.net/appdatabase")
-db = client.get_database("appdatabase")
+news_tables = {}
 
-@app.route('https://127.0.0.1:8000', methods=['OPTIONS'])
-def handle_preflight():
-    response = make_response()
-    response.headers["Access-Control-Allow-Origin"] = "*"
-    response.headers["Access-Control-Allow-Methods"] = "POST"
-    response.headers["Access-Control-Allow-Headers"] = "Content-Type"
-    return response
+for ticker in tickers:
+    url = finviz_url + ticker
 
-@app.route('https://127.0.0.1:8000', methods=['POST'])
+    req = Request(url=url, headers={'user-agent': 'my-app'})
+    response = urlopen(req)
 
-def predict():
-    if request.method == 'POST':
-        data = request.get_json()
-        print(data)
-        result = {'prediction': 'niggaooooo '}
-        return jsonify(result)
+    html = BeautifulSoup(response, 'html')
+    news_table = html.find(id='news-table')
+    news_tables[ticker] = news_table
+
+parsed_data = []
+
+for ticker, news_table in news_tables.items():
     
-if __name__ == "__main__":
-    app.run(host="127.0.0.1", port=8000)
+        for row in news_table.findAll('tr'):
+    
+            title = row.a.text
+            date_data = row.td.text.split(' ')
+    
+            if len(date_data) == 1:
+                time = date_data[0]
+            else:
+                date = date_data[0]
+                time = date_data[1]
+    
+            parsed_data.append([ticker, date, time, title])
+
+df = pd.DataFrame(parsed_data, columns=['ticker', 'date', 'time', 'title'])
+
+vader = SentimentIntensityAnalyzer()
+
+f = lambda title: vader.polarity_scores(title)['compound']
+df['compound'] = df['title'].apply(f)
+df['date'] = pd.to_datetime(df.date).dt.date
+
+plt.figure(figsize=(10,8))
+
+mean_df = df.groupby(['ticker', 'date']).mean().unstack()
+mean_df = mean_df.xs('compound', axis='columns').transpose()
+mean_df.plot(kind='bar')
+plt.show()
+
+
+
+
+
+
